868e0816b3544244634e2f4c98879b838be7884e
tech-tron <phillables@gmail.com>
2022-04-13 13:34:55 -0400

    main manager added to presist from main menu
    MainManager empty and script added to menu scene that destoys itself if it already exist. dont destroy on load and gets an instance of itself

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,8 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene(0);
+    }
 }

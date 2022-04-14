eb3e38648ed442821010c67efe0fc3a3a352517c
tech-tron <phillables@gmail.com>
2022-04-14 07:21:28 -0400

    save team color in mainmanager script
    create a serializable class in main manager script that has a color type. add SaveColor function in main manager class that creates an instance of a save data and sets that team color to the main managers team color then puts it to jason data saved in the presistent path. the load color just does this backwards after checking to see if the file exist

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MainManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // Start() and Update() methods deleted - we dont need them
+
+    public static MainManager Instance;
+    public Color TeamColor;
+
+    private void Awake(){
+        if(Instance != null){
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [System.Serializable]
+    class SaveData{
+        public Color TeamColor;
+    }
+
+    public void SaveColor(){
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data); 
+        File.WriteAllText(Application.persistentDataPath +"/savefile.json", json);
+    }
+
+    public  void LoadColor(){
+        string path = Application.persistentDataPath + "/savefile.json";
+        if(File.Exists(path)){
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
     }
 }
